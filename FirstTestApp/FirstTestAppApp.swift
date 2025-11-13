bb12247af8cf4df947ee2a77d3e3cec33373b7b2
Imron <imronimronov32@gmail.com>
2025-11-13 16:30:22 +0500

    Initial Commit

---
+++ FirstTestApp/FirstTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstTestAppApp.swift
+//  FirstTestApp
+//
+//  Created by Imron Mirzarasulov on 13/11/25.
+//
+
+import SwiftUI
+
+@main
+struct FirstTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
