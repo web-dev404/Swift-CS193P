0c3586217bd595e684efd11302b8ecbe7e9d1724
Imron <imronimronov32@gmail.com>
2025-11-14 22:33:41 +0500

    Initial commit

--- FirstTestApp/ContentView.swift
+++ FirstTestApp/ContentView.swift
@@ -1,24 +1,43 @@
-//
-//  ContentView.swift
-//  FirstTestApp
-//
-//  Created by Imron Mirzarasulov on 13/11/25.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack(spacing: 20) {
             Image(systemName: "globe")
                 .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .foregroundStyle(.blue)
+            Text("KAIF RESIDENCE")
         }
         .padding()
+        HStack(spacing: 20) {
+            CardView(isFaceUp: true)
+            CardView()
+            Image(systemName: "phone")
+                .imageScale(.small)
+            Text("PHONE NUMBER")
+        }
+        .padding(10)
+        .foregroundColor(.orange)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack(content: {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12).foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 2)
+                Text("YO").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+            }
+        })
     }
 }
 
 #Preview {
     ContentView()
 }
+
