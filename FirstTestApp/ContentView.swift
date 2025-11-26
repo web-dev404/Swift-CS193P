6cad6ca52feb592befb638213833c21be9611e84
Imron <imronimronov32@gmail.com>
2025-11-26 22:20:30 +0500

    Implemented required logic

--- FirstTestApp/ContentView.swift
+++ FirstTestApp/ContentView.swift
@@ -1,43 +1,76 @@
 import SwiftUI
 
 struct ContentView: View {
+    let cardsList = ["A", "B", "C", "D", "E", "F", "G"]
+    
+    @State var cardCount: Int = 2
+    
     var body: some View {
-        VStack(spacing: 20) {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.blue)
-            Text("KAIF RESIDENCE")
+        VStack {
+            ScrollView {
+                cards
+            }
+            
+            Spacer()
+            
+            HStack {
+                cardAdder
+                Spacer()
+                cardRemover
+            }
+            .font(.largeTitle)
         }
         .padding()
-        HStack(spacing: 20) {
-            CardView(isFaceUp: true)
-            CardView()
-            Image(systemName: "phone")
-                .imageScale(.small)
-            Text("PHONE NUMBER")
+    }
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 100, maximum: 200))]) {
+            ForEach(0..<cardCount, id: \.self) { index in
+                CardView(cardText: cardsList[index], isFaceUp: true)
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
         }
-        .padding(10)
-        .foregroundColor(.orange)
+    }
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > cardsList.count)
+    }
+    
+    var cardRemover: some View {
+        return cardCountAdjuster(by: -1, symbol: "minus")
+    }
+    
+    var cardAdder: some View {
+        return cardCountAdjuster(by: +1, symbol: "plus")
     }
 }
 
 struct CardView: View {
-    var isFaceUp: Bool = false
-    
+    let cardText: String
+    @State var isFaceUp = false
+
     var body: some View {
-        ZStack(content: {
-            if isFaceUp {
-                RoundedRectangle(cornerRadius: 12).foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 2)
-                Text("YO").font(.largeTitle)
-            } else {
-                RoundedRectangle(cornerRadius: 12)
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            Group {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(cardText).font(.largeTitle)
             }
-        })
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
     }
 }
 
 #Preview {
     ContentView()
 }
-
